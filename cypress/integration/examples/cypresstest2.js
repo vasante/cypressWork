75149cecdccbed11b624def2f8bfe281906edafd
vasante <vasantepannirselvam@gmail.com>
2020-09-23 20:42:04 +0200

    testing

--- cypress/integration/examples/cypresstest2.js
+++ cypress/integration/examples/cypresstest2.js
@@ -13,6 +13,8 @@ describe('My First Cypress Test', function() {
     cy.get(':nth-child(1) > .element-list > .menu-list > #item-0').click();
   
     cy.get("#userName").type("sfsfsf");
+
+    
    
   })
   })
